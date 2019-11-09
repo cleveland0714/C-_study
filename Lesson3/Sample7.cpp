8821fe81a41717dce01015c7506adc282842c044
Cleveland0714 <45794286+cleveland0714@users.noreply.github.com>
2019-11-09 19:09:03 +0900

    Create Sample7.cpp

---
+++ Lesson3/Sample7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n1,n2;
+	n1 = 3;
+	cout << "変数numの値は" << n1 << "です\n";
+	n2 = n1;
+	cout << "n1の値をn2に代入\n";
+	cout << "n2の値は" << n2 << '\n';
+	return 0;
+}
